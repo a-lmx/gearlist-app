045cb6f441a843534dacacee23b52fb040b36ef8
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-20 13:30:27 -0700

    added #list_details and #section functions to get data; WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,18 @@
 class HomeController < ApplicationController
   def index
-    url = 'http://localhost:3000/api/v1/lists'
-    @lists = HTTParty.get(url)
+    @list = list_details(3)
+    @sections = sections(3)
+  end
+
+  def list_details(list_id)
+    url = 'http://localhost:3000/api/v1/lists/' + list_id.to_s
+    response = HTTParty.get(url)
+    return response.parsed_response
+  end
+
+  def sections(list_id)
+    url = 'http://localhost:3000/api/v1/lists/' + list_id.to_s + '/sections'
+    response = HTTParty.get(url)
+    return response.parsed_response
   end
 end
