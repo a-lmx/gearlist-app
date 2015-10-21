3b60708989a566d8e36032740cbf63ebefb3326b
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-21 16:44:24 -0700

    added items route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'lists#index'
 
-  resources :lists, only: [:index, :show, :new, :create]
+  resources :lists, only: [:index, :show, :new, :create] do
+    get '/items', to: 'items#add', as: 'items'
+  end
 end
