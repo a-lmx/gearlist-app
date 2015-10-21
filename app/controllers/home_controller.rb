b63d508ee29054c871b5d119b42d45b9d199d0ee
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-21 10:52:02 -0700

    removed comments from scaffolding #build_list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,2 @@
 class HomeController < ApplicationController
 end
-
-# to display list:
-# => get list details
-  # => get '/lists/:id'
-# => get list_sections
-  # => get 'lists/:id/sections'
-# => for each list_section
-  # => get list_section_items
-    # => get 'list-sections/:id/items'
-  # => subtotal section
-# => calculate total weight of all list_sections_items
