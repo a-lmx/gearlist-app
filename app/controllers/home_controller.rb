bfd0843888f8b889b4b9a155c543d76c16ab55bd
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-20 19:12:07 -0700

    tinkering with #index action; scaffolding List object; WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,18 @@
 class HomeController < ApplicationController
   def index
     @list = list_details(3)
-    @sections = sections(3)
+    @complete_list = list_items(3)
+    # @sections = sections(3)
   end
 
   def list_details(list_id)
     url = 'http://localhost:3000/api/v1/lists/' + list_id.to_s
+    response = HTTParty.get(url)
+    return response.parsed_response
+  end
+
+  def list_items(list_id)
+    url = 'http://localhost:3000/api/v1/lists/' + list_id.to_s + '/items_by_section'
     response = HTTParty.get(url)
     return response.parsed_response
   end
@@ -16,3 +23,11 @@ class HomeController < ApplicationController
     return response.parsed_response
   end
 end
+
+# to display list:
+# => get list details
+# => get list_sections
+# => for each list_section
+  # => get list_section_items
+  # => subtotal section
+# => calculate total weight of all list_sections_items
