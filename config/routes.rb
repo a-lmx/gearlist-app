3801d36c8f9240b9b9a529ad2bc49da2e4be7c1b
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-21 09:51:34 -0700

    added routes for new_list & create_list

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get '/lists/:id', to: 'home#show', as: 'list' 
-  get 'login', to: 'home#login', as: 'login'
+
+  get '/lists/new', to: 'home#new_list', as: 'new_list'
+  post '/lists/new', to: 'home#create_list', as: 'create_list'
+
+  # get 'login', to: 'home#login', as: 'login'
 end
