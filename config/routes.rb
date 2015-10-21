ccc27ee8c24c73ec423a6f434d11f228147cf9cc
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-21 09:51:51 -0700

    removed as-yet unused login route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,4 @@ Rails.application.routes.draw do
 
   get '/lists/new', to: 'home#new_list', as: 'new_list'
   post '/lists/new', to: 'home#create_list', as: 'create_list'
-
-  # get 'login', to: 'home#login', as: 'login'
 end
