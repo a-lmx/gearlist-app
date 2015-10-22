21edc09a166d924830c22f63cf4da7af2c5b76aa
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-21 19:36:12 -0700

    added :list_id to attributes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item
   include ActiveModel::Model
 
-  attr_accessor :name, :category, :weight, :quantity, :section
+  attr_accessor :name, :category, :weight, :quantity, :section, :list_id
 
-  validates :name, :category, :weight, :quantity, :section, presence: true
+  validates :name, :category, :weight, :quantity, :section, :list_id, presence: true
 end
