1c2d360d0e3dd2915b8e925b3ce248ebf47d5f7e
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-21 19:37:01 -0700

    added #create action and #item_params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,33 @@
 class ItemsController < ApplicationController
   def new
     @item = Item.new
+    @list_id = params[:list_id]
     render :new
+  end
+
+  def create
+    list_id = params["item"]["list_id"]
+    url = ApplicationController::BASE_URI + '/lists/'+ list_id + '/add_items'
+    options = {
+      body: {
+        item: item_params
+      }
+    }
+    raise
+    response = HTTParty.post(url, options)
+    # contents = response.parsed_response
+
+    # if contents["success"]
+    #   flash message yay
+    # else
+    #   flash message wah
+    # end
+    redirect_to list_path(list_id)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:section, :category, :name, :quantity, :weight, :list_id)
   end
 end
