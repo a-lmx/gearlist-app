90f1c536c475fbb35e73b682c6ed8265554e82ce
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-22 21:49:46 -0700

    created #create method to assign session data after log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,12 @@ class SessionsController < ApplicationController
   def new
     render :new
   end
+
+  def create
+    session[:user_id] = params["userId"]
+    session[:token] = params["token"]
+
+    render  json: { success: "You set the session user_id and token." }, 
+            status: 200
+  end
 end
