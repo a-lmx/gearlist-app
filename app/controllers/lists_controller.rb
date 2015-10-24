daf092cdb1687c8c3cb4b8532936f8c40428b48c
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-24 16:49:19 -0700

    moved #get_list_details to ApplicationController

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -103,12 +103,6 @@ class ListsController < ApplicationController
     return lists
   end
 
-  def get_list_details(list_id)
-    url = ApplicationController::BASE_URI + '/lists/' + list_id.to_s
-    response = HTTParty.get(url, headers: auth_header)
-    return response.parsed_response
-  end
-
   def get_list_sections(list_id)
     url = ApplicationController::BASE_URI + '/lists/' + list_id.to_s + '/sections'
     response = HTTParty.get(url, headers: auth_header)
