e1430819af81f93672320e60babc367a685da372
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-24 13:21:58 -0700

    added restriction to #edit to limit to lists' owners

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,15 @@
 class ListsController < ApplicationController
+  MESSAGES = {
+    # create_success: "You have successfully created a new recipe.",
+    # create_fail: "There was a problem with your new recipe. Please try again.",
+    # update_success: "You have successfully updated your recipe.",
+    # update_fail: "There was a problem with your update. Please try again.",
+    # destroy_success: "You have successfully deleted the recipe.",
+    # destroy_fail: "There was a problem with your recipe deletion. Please try again.",
+    # cookbook_fail: "Saving this recipe to that cookbook did not work. Please try again. ",
+    not_yo_list: "You cannot edit someone else's Gear List."
+  }
+
   def index
     @lists = get_lists
   end
@@ -32,6 +43,12 @@ class ListsController < ApplicationController
 
   def edit
     list_info = get_list_details(params[:id])
+
+    if list_info['user_id'] != session[:user_id]
+      flash[:errors] = MESSAGES[:not_yo_list]
+      redirect_to root_path
+    end
+
     @list = List.new(
       name: list_info['name'], 
       description: list_info['description'], 
@@ -41,7 +58,6 @@ class ListsController < ApplicationController
     )
     @user_id = session[:user_id]
     @submit_text = 'Update Gear List'
-    render :edit
   end
 
   def update
