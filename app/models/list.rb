952531b341c5ecdff1089c15a1b65f2a7a3e273b
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-24 12:09:32 -0700

    added :id attribute to List model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List
   include ActiveModel::Model
 
-  attr_accessor :name, :description, :secret, :user_id
+  attr_accessor :name, :description, :secret, :user_id, :id
 
   validates :name, :user_id, presence: true
 end
