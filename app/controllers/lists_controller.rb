e0318eb551bbc35063baf058931cf90eb5a45402
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-24 15:47:55 -0700

    removed Lists::MESSAGES

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,4 @@
 class ListsController < ApplicationController
-  MESSAGES = {
-    # create_success: "You have successfully created a new recipe.",
-    # create_fail: "There was a problem with your new recipe. Please try again.",
-    # update_success: "You have successfully updated your recipe.",
-    # update_fail: "There was a problem with your update. Please try again.",
-    # destroy_success: "You have successfully deleted the recipe.",
-    # destroy_fail: "There was a problem with your recipe deletion. Please try again.",
-    # cookbook_fail: "Saving this recipe to that cookbook did not work. Please try again. ",
-    not_yo_list: "You cannot edit someone else's Gear List."
-  }
-
   def index
     @lists = get_lists
   end
@@ -43,7 +32,7 @@ class ListsController < ApplicationController
     list_info = get_list_details(params[:id])
 
     unless list_info['user_id'].to_s == @current_user_id
-      flash[:errors] = MESSAGES[:not_yo_list]
+      flash[:errors] = ApplicationController::MESSAGES[:not_yo_list]
       redirect_to root_path
     end
 
