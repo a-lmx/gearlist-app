2e29129bcd1748a3241f3ef8caf21b78fda1c1e6
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-26 14:46:21 -0700

    added custom domain name to BASE_URI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :user_lists, :current_user_id, :require_signin
 
-  BASE_URI = 'http://localhost:3000/api/v1'
+  # BASE_URI = 'http://localhost:3000/api/v1'
+  BASE_URI = 'http://www.penguingearlist.com'+'/api/v1'
 
   MESSAGES = {
     not_yo_list: "You cannot edit someone else's Gear List."
