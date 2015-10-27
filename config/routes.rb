131386a5fcc5498b910e32c8c13a6e524b632223
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-27 15:01:49 -0700

    added route for items#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'lists#index'
 
   resources :lists, only: [:index, :show, :new, :create, :edit, :update] do
-    resources :items, only: [:new, :create, :edit, :update]
+    resources :items, only: [:new, :create, :edit, :update, :destroy]
   end
 
   get '/login', to: 'sessions#new', as: 'login'
