c014b107a4d6b21d0a001023cfc2cc9b57ad7156
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-28 13:52:05 -0700

    added items search route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root 'lists#index'
 
   resources :lists, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    get '/items/search', to: 'items#search', as: 'search_items'
     resources :items, only: [:new, :create, :edit, :update, :destroy]
   end
+
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
