1ae5696cf8af81667cfd83edfe941f64ab02e50c
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-29 13:27:52 -0700

    created initialize method, moved over get_user_lists

--- lib/gearlist_api.rb
+++ lib/gearlist_api.rb
@@ -1,3 +1,21 @@
 class GearlistApi
-  
+  attr_accessor :auth_header
+
+  if Rails.env.production?
+    GEARLIST_URI = 'http://gearlist-api-prod.elasticbeanstalk.com/api/v1'
+  else
+    GEARLIST_URI = 'http://localhost:3000/api/v1'
+  end
+
+  def initialize(token)
+    @auth_header = {
+      "Authorization" => "Token token=\"#{token}\""
+    }
+  end
+
+  def get_user_lists(user_id)
+    url = GEARLIST_URI + '/users/' + user_id.to_s + '/lists'
+    response = HTTParty.get(url, headers: auth_header)
+    return response.parsed_response
+  end
 end
