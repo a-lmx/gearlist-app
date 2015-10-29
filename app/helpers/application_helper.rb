6f9be8e3652ac00753d9aeaf25fe45342017367e
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-29 11:39:41 -0700

    added gms_to_lbs_oz

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,20 @@ module ApplicationHelper
   end
 
   def gms_to_oz(gms)
-    number_with_precision((gms * 0.035274), precision: 1)
+    (gms * 0.035274).round(1)
+  end
+
+  def gms_to_lbs_oz(gms)
+    total_ounces = gms_to_oz(gms)
+    lbs = (total_ounces / 16).floor
+    if lbs > 0
+      lb_form = lbs == 1 ? 'lb.' : 'lbs.'
+      ounces = (total_ounces % 16).round(1)
+      lbs_oz = "#{lbs} #{lb_form} #{ounces} oz."
+    else
+      lbs_oz = "#{total_ounces} oz."
+    end
+
+    lbs_oz
   end
 end
